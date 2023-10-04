29b840430a2b850e3baf104931ed1f1bce6181a3
yifeng <jerryuhoo@gmail.com>
2023-10-04 09:54:28 -0400

    update psycho acoustic loss

--- asteroid/losses/psycho_acoustic_loss.py
+++ asteroid/losses/psycho_acoustic_loss.py
@@ -7,54 +7,162 @@ import matplotlib.pyplot as plt
 # from MDCTfb import *
 
 
-def psycho_acoustic_loss(y_pred, y_true, fs=44100, N=1024, nfilts=64, quality=100):
-    mT_pred, mTbarkquant_pred = compute_masking_threshold(
-        y_pred.squeeze(), fs, N, nfilts, quality
-    )
-    mT_true, mTbarkquant_true = compute_masking_threshold(
-        y_true.squeeze(), fs, N, nfilts, quality
-    )
+def psycho_acoustic_loss(ys_pred, ys_true, fs=44100, N=1024, nfilts=64, quality=100):
+    # Assuming y_pred and y_true have shape: (batch_size, channels, nfft, frame_length)
 
-    mse = F.mse_loss(mTbarkquant_pred, mTbarkquant_true)
-    return mse
+    # Check the number of channels (either 1 for mono or 2 for stereo)
+    channels = ys_pred.shape[1]
+
+    if channels not in [1, 2]:
+        raise ValueError(
+            f"Unsupported number of channels: {channels}, only mono and stereo are supported"
+        )
+
+    # Function to compute MSE loss for a single channel
+    def compute_channel_loss(y_pred_channel, y_true_channel):
+        mT_pred, mTbarkquant_pred = compute_masking_threshold(
+            y_pred_channel, fs, N, nfilts, quality
+        )
+        mT_true, mTbarkquant_true = compute_masking_threshold(
+            y_true_channel, fs, N, nfilts, quality
+        )
+        # print("mT_pred", mT_pred.shape)
+        # print("mT_true", mT_true.shape)
+        return F.mse_loss(mTbarkquant_pred, mTbarkquant_true)
+
+    if channels == 1:
+        # Mono audio
+        mse_loss = compute_channel_loss(ys_pred, ys_true)
+    else:
+        # Stereo audio
+        mse_left = compute_channel_loss(ys_pred[:, 0, :, :], ys_true[:, 0, :, :])
+        mse_right = compute_channel_loss(ys_pred[:, 1, :, :], ys_true[:, 1, :, :])
+        mse_loss = (mse_left + mse_right) / 2  # Average loss across channels
+
+    return mse_loss
 
 
-def compute_masking_threshold(x, fs, N, nfilts=64, quality=100):
+def get_analysis_params(fs, N, nfilts=64):
     maxfreq = fs / 2
     alpha = 0.8  # Exponent for non-linear superposition of spreading functions
     nfft = 2 * N  # number of fft subbands
 
     W = mapping2barkmat(fs, nfilts, nfft)
-    W_inv = mappingfrombarkmat(W, nfft)
     spreadingfunctionBarkdB = f_SP_dB(maxfreq, nfilts)
     spreadingfuncmatrix = spreadingfunctionmat(spreadingfunctionBarkdB, alpha, nfilts)
 
-    # Compute STFT
-    ys = torch.stft(x, n_fft=2 * N, return_complex=True)
-    ys = ys * torch.sqrt(torch.tensor(2 * N / 2)) / 2 / 0.375
+    return W, spreadingfuncmatrix, alpha
+
+
+"""
+def compute_masking_threshold(x, fs, N, nfilts=64, quality=100):
+    W, spreadingfuncmatrix, alpha = get_analysis_params(fs, N, nfilts)
+    ys = compute_STFT(x, N)
+    W = W.to(ys.device)
 
     M = ys.shape[1]  # number of blocks in the signal
     mT = torch.zeros((N + 1, M))
-    mTbarkquant = torch.zeros((nfilts, M))
-
-    # print("ys", ys.shape)
-    # print("torch.abs(ys)", torch.abs(ys).shape)
-    # print("torch.abs(ys[:, m])", torch.abs(ys[:, 0]).shape)
-    # print("W", W.shape)
 
     for m in range(M):  # M: number of blocks
-        mXbark = mapping2bark(torch.abs(ys[:, m]), W, nfft)
+        mXbark = mapping2bark(torch.abs(ys[:, m]), W, 2 * N)
+        print("mXbark", mXbark)
         mTbark = maskingThresholdBark(
             mXbark, spreadingfuncmatrix, alpha, fs, nfilts
         ) / (quality / 100)
+        print("mTbark", mTbark)
 
+        # Skip the quantization steps
+        # Directly map mTbark to mT
+        mTbark = mTbark.squeeze(0)
+        mT[:, m] = mappingfrombark(mTbark, mappingfrombarkmat(W, 2 * N), 2 * N)
+
+    return mT, 0
+"""
+
+
+# def compute_masking_threshold(ys, fs, N, nfilts=64, quality=100):
+#     W, spreadingfuncmatrix, alpha = get_analysis_params(fs, N, nfilts)
+
+#     W = W.to(ys.device)
+
+#     M = ys.shape[1]  # number of blocks in the signal
+#     mT = torch.zeros((N + 1, M))
+#     mTbarkquant = torch.zeros((nfilts, M))
+
+#     for m in range(M):  # M: number of blocks
+#         mXbark = mapping2bark(torch.abs(ys[:, m]), W, 2 * N)
+#         mTbark = maskingThresholdBark(
+#             mXbark, spreadingfuncmatrix, alpha, fs, nfilts
+#         ) / (quality / 100)
+#         mTbarkquant[:, m] = torch.round(torch.log2(mTbark) * 4)
+#         mTbarkquant[:, m].clamp_(min=0)
+#         mTbarkdequant = torch.pow(2, mTbarkquant[:, m] / 4).to(mXbark.device)
+#         mT[:, m] = mappingfrombark(mTbarkdequant, mappingfrombarkmat(W, 2 * N), 2 * N)
+
+#     return mT, mTbarkquant
+
+
+# def compute_masking_threshold(ys, fs, N, nfilts=64, quality=100):
+#     W, spreadingfuncmatrix, alpha = get_analysis_params(fs, N, nfilts)
+#     W = W.to(ys.device)
+#     ys = ys.squeeze()
+#     M = ys.shape[1]  # number of blocks in the signal
+#     mT = torch.zeros((N + 1, M))
+#     mTbarkquant = torch.zeros((nfilts, M))
+
+#     for m in range(M):  # M: number of blocks (frame number)
+#         mXbark = mapping2bark(torch.abs(ys[:, m]), W, 2 * N)
+#         mTbark = maskingThresholdBark(
+#             mXbark, spreadingfuncmatrix, alpha, fs, nfilts
+#         ) / (quality / 100)
+#         mTbarkquant[:, m] = torch.round(torch.log2(mTbark) * 4)
+#         mTbarkquant[:, m].clamp_(min=0)
+#         mTbarkdequant = torch.pow(2, mTbarkquant[:, m] / 4).to(mXbark.device)
+#         mT[:, m] = mappingfrombark(mTbarkdequant, mappingfrombarkmat(W, 2 * N), 2 * N)
+#         print("mTbark", mTbark)
+#         print("mTbarkquant", mTbarkquant)
+#         print("mTbarkdequant", mTbarkdequant)
+#         print("mT", mT)
+#     return mT, mTbarkquant
+
+
+def compute_masking_threshold(ys, fs, N, nfilts=64, quality=100):
+    W, spreadingfuncmatrix, alpha = get_analysis_params(fs, N, nfilts)
+    W = W.to(ys.device)
+    ys = ys.squeeze()
+    M = ys.shape[1]  # number of blocks in the signal
+    mT = torch.zeros((N + 1, M))
+
+    # for m in range(M):  # M: number of blocks (frame number)
+    #     mXbark = mapping2bark(torch.abs(ys[:, m]), W, 2 * N)
+    #     mTbark = maskingThresholdBark(
+    #         mXbark, spreadingfuncmatrix, alpha, fs, nfilts
+    #     ) / (quality / 100)
+    # print("mTbark", mTbark)
+    # return mTbark, 0
+
+    mTbarkquant = torch.zeros((nfilts, M))
+
+    for m in range(M):  # M: number of blocks (frame number)
+        mXbark = mapping2bark(torch.abs(ys[:, m]), W, 2 * N)
+        mTbark = maskingThresholdBark(
+            mXbark, spreadingfuncmatrix, alpha, fs, nfilts
+        ) / (quality / 100)
         mTbarkquant[:, m] = torch.round(torch.log2(mTbark) * 4)
         mTbarkquant[:, m].clamp_(min=0)
-
-        mTbarkdequant = torch.pow(2, mTbarkquant[:, m] / 4)
-        mT[:, m] = mappingfrombark(mTbarkdequant, W_inv, nfft)
-
+        mTbarkdequant = torch.pow(2, mTbarkquant[:, m] / 4).to(mXbark.device)
+        mT[:, m] = mappingfrombark(mTbarkdequant, mappingfrombarkmat(W, 2 * N), 2 * N)
+        # print("mTbark", mTbark)
+        # print("mTbarkquant", mTbarkquant)
+        # print("mTbarkdequant", mTbarkdequant)
+        # print("mT", mT)
     return mT, mTbarkquant
+
+
+def compute_STFT(x, N):
+    ys = torch.stft(x, n_fft=2 * N, return_complex=True)
+    ys = ys * torch.sqrt(torch.tensor(2 * N / 2)) / 2 / 0.375
+    return ys
 
 
 def gaussian_spreading_function(nfilts, sigma):
@@ -92,6 +200,7 @@ def spreadingfunctionmat(spreadingfunctionBarkdB, alpha, nfilts):
 
 
 def maskingThresholdBark(mXbark, spreadingfuncmatrix, alpha, fs, nfilts):
+    spreadingfuncmatrix = spreadingfuncmatrix.to(mXbark.device)
     mTbark = torch.mm(mXbark**alpha, spreadingfuncmatrix**alpha)
     mTbark = mTbark ** (1.0 / alpha)
 
@@ -109,7 +218,7 @@ def maskingThresholdBark(mXbark, spreadingfuncmatrix, alpha, fs, nfilts):
         ),
         -20,
         120,
-    )
+    ).to(mXbark.device)
     mTbark = torch.max(mTbark, 10.0 ** ((LTQ - 60) / 20))
     return mTbark
 
@@ -147,6 +256,31 @@ def mapping2bark(mX, W, nfft):
     return mXbark
 
 
+# def mapping2bark(mX, W, nfft):
+#     # Assuming mX has shape (batch_size, nfreqs)
+#     # and W has shape (nfilts, nfreqs)
+
+#     # Expand dimensions of mX to have shape (batch_size, 1, nfreqs)
+#     mX_expanded = mX.unsqueeze(1)
+
+#     # Transpose W to have shape (nfreqs, nfilts),
+#     # then expand dimensions to have shape (1, nfilts, nfreqs)
+#     W_expanded = W.t().unsqueeze(0)
+
+#     # Now, we can perform batch matrix multiplication
+#     # The resulting shape will be (batch_size, nfilts, 1)
+#     mXbark = torch.bmm(
+#         W_expanded.expand(mX_expanded.size(0), -1, -1), mX_expanded.transpose(1, 2)
+#     )
+
+#     # Take square, then square root, and squeeze the last dimension
+#     # to get final shape of (batch_size, nfilts)
+#     mXbark = (mXbark**2.0) ** 0.5
+#     mXbark = mXbark.squeeze(-1)
+
+#     return mXbark
+
+
 def mappingfrombarkmat(W, nfft):
     nfreqs = int(nfft / 2)
     W_inv = torch.mm(
@@ -160,3 +294,90 @@ def mappingfrombark(mTbark, W_inv, nfft):
     nfreqs = int(nfft / 2)
     mT = torch.mm(mTbark, W_inv[:, :nfreqs].T)
     return mT
+
+
+# def mappingfrombarkmat(W, nfft):
+#     nfreqs = int(nfft / 2)
+#     W_inv = torch.bmm(
+#         torch.diag_embed(1.0 / (torch.sum(W, dim=1) + 1e-6)).sqrt(),
+#         W[:, 0 : nfreqs + 1]
+#         .unsqueeze(0)
+#         .expand(W.size(0), *W[:, 0 : nfreqs + 1].shape),
+#     ).permute(0, 2, 1)
+#     return W_inv
+
+
+# def mappingfrombark(mTbark, W_inv, nfft):
+#     # Assuming mTbark has shape (batch_size, nfilts, M)
+#     nfreqs = int(nfft / 2)
+#     mT = torch.bmm(mTbark, W_inv[:, :nfreqs].permute(0, 2, 1))
+#     return mT
+
+
+def plot_results(ys, fs, N, nfilts=64, quality=100):
+    mT, mTbarkquant = compute_masking_threshold(ys, fs, N, nfilts, quality)
+
+    # Convert STFT magnitude to dB for visualization
+    ys_dB = 20 * torch.log10(torch.abs(ys) + 1e-6)
+    # Convert masking threshold to dB for visualization
+    mT_dB = 20 * torch.log10(mTbarkquant + 1e-6)
+
+    print("mt_dB", mT_dB)
+    print("ys_dB", ys_dB)
+
+    # Frequency and Time vectors for plotting
+    f = np.linspace(0, fs / 2, ys.shape[0])
+    t = np.linspace(0, ys.shape[0], ys.shape[1])
+
+    # Plotting
+    plt.figure(figsize=(12, 8))
+
+    # Plot Spectrogram
+    plt.subplot(3, 1, 1)
+    plt.pcolormesh(t, f, ys_dB.numpy(), shading="gouraud", vmin=0, vmax=60)
+    plt.colorbar(label="dB")
+    plt.title("Spectrogram")
+    plt.ylabel("Frequency (Hz)")
+
+    # Plot Spectrum and Masking Threshold of Middle Frame
+    middle_frame_idx = len(t) // 2
+    plt.subplot(3, 1, 2)
+    print("ys", ys_dB[:, middle_frame_idx].numpy())
+    print("mt", mT_dB[:, middle_frame_idx].numpy())
+    plt.plot(
+        f, ys_dB[:, middle_frame_idx].numpy(), color="blue", label="Spectrum", alpha=0.7
+    )
+    plt.plot(
+        f,
+        mT_dB[:, middle_frame_idx].numpy(),
+        color="red",
+        label="Masking Threshold",
+        alpha=0.7,
+    )
+    plt.legend()
+    plt.title(f"Spectrum and Masking Threshold at t = {t[middle_frame_idx]:.2f} s")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Amplitude (dB)")
+
+    # Overlay bark scale center frequencies in blue
+    W, _, alpha = get_analysis_params(fs, N, nfilts)
+    bark_center_freqs = bark2hz(np.linspace(0, hz2bark(fs / 2), W.shape[0]))
+    # for freq in bark_center_freqs:
+    #     plt.axhline(y=freq, color="blue", linewidth=0.5, alpha=0.7)
+
+    # Plot Spreading Function
+    spreadingfunctionBarkdB = f_SP_dB(fs / 2, W.shape[0])
+    # print("spreadingfunctionBarkdB", spreadingfunctionBarkdB)
+    spreadingfunctionBarkVoltage = 10.0 ** (spreadingfunctionBarkdB / 20.0 * alpha)
+
+    plt.subplot(3, 1, 3)
+    plt.plot(spreadingfunctionBarkVoltage.numpy())
+    x_length = len(spreadingfunctionBarkVoltage)
+    plt.axvline(x=x_length // 2, color="red", linestyle="--")
+    plt.axvline(x=x_length - 1, color="red", linestyle="--")
+    plt.title("Spreading Function")
+    plt.xlabel("Bark Scale")
+    plt.ylabel("Amplitude (Voltage)")
+
+    plt.tight_layout()
+    plt.savefig("spectrogram_with_masking_threshold.png")
--- asteroid/losses/psycho_acoustic_test.py
+++ asteroid/losses/psycho_acoustic_test.py
@@ -1,6 +1,10 @@
 import torchaudio
 import torch
-from asteroid.losses.psycho_acoustic_loss import psycho_acoustic_loss, plot_results
+from asteroid.losses.psycho_acoustic_loss import (
+    psycho_acoustic_loss,
+    plot_results,
+    compute_STFT,
+)
 
 waveform, sample_rate = torchaudio.load("test48khz.wav")
 y_true = waveform[0]
@@ -8,18 +12,30 @@ y_true = waveform[0]
 noise = 0.01 * torch.randn(y_true.size())
 y_noisy = y_true + noise
 
-loss_noisy = psycho_acoustic_loss(y_noisy, y_true, fs=sample_rate)
-print(f"Psychoacoustic Loss (with noise): {loss_noisy.item()}")
-
 y_random1 = torch.randn(y_true.size())
 y_random2 = torch.randn(y_true.size())
 
-# loss_self = psycho_acoustic_loss(y_true, y_true, fs=sample_rate)
-# loss_random1 = psycho_acoustic_loss(y_random1, y_true, fs=sample_rate)
-# loss_random2 = psycho_acoustic_loss(y_random2, y_true, fs=sample_rate)
+ys_noisy = compute_STFT(y_noisy, N=1024).unsqueeze(0).unsqueeze(0)
+ys_true = compute_STFT(y_true, N=1024).unsqueeze(0).unsqueeze(0)
+ys_random1 = compute_STFT(y_random1, N=1024).unsqueeze(0).unsqueeze(0)
+ys_random2 = compute_STFT(y_random2, N=1024).unsqueeze(0).unsqueeze(0)
 
-# print(f"Psychoacoustic Loss (self): {loss_self.item()}")
-# print(f"Psychoacoustic Loss (random noise 1): {loss_random1.item()}")
-# print(f"Psychoacoustic Loss (random noise 2): {loss_random2.item()}")
+print("ys_noisy", ys_noisy.shape)
 
-plot_results(y_true, sample_rate, 1024)
+loss_noisy = psycho_acoustic_loss(ys_noisy, ys_true, fs=sample_rate)
+print(f"Psychoacoustic Loss (with noise): {loss_noisy.item()}")
+
+
+loss_self = psycho_acoustic_loss(ys_true, ys_true, fs=sample_rate)
+loss_random1 = psycho_acoustic_loss(ys_random1, ys_true, fs=sample_rate)
+loss_random2 = psycho_acoustic_loss(ys_random2, ys_true, fs=sample_rate)
+
+print(f"Psychoacoustic Loss (self): {loss_self.item()}")
+print(f"Psychoacoustic Loss (random noise 1): {loss_random1.item()}")
+print(f"Psychoacoustic Loss (random noise 2): {loss_random2.item()}")
+
+# y_true = y_true.unsqueeze(0)
+
+# N = 1024
+# ys_true = compute_STFT(y_true, N)
+# plot_results(ys_true, sample_rate, N)
